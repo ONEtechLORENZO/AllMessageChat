57be4a1a2bc49b195b63dc6993b2abf6453cc1e9
Marat <marat.yerkebayev@aessefin.it>
2026-03-06 14:57:16 +0100

    feat: changed favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title inertia>{{ config('app.name', 'OneMessage') }}</title>
+    <!-- Favicon -->
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="icon" href="/favicon.png" type="image/png">
+    <link rel="apple-touch-icon" href="/favicon.png">
 
     @routes
     @viteReactRefresh
